761a985a4bac93f07c9a64874793eda74f7a039a
Michał Kolczyk <kolczykm@gmail.com>
2011-12-29 16:09:01 +0100

    Ustawienia projektu, dodanie pliku który będzie zawierać testy.

---
+++ Tests.cpp
@@ -0,0 +1,11 @@
+/*
+ * Tests.cpp
+ *
+ *  Created on: 29-12-2011
+ *      Author: Michal
+ */
+
+#define BOOST_TEST_MODULE WaveletTransformCompressionTest
+#include <boost/test/included/unit_test.hpp>
+
+
