0214579a290329528b3e0072efafc28a249a034d
Michał Kolczyk <kolczykm@gmail.com>
2011-12-30 10:41:27 +0100

    Testy klasy UnsignedInteger.

--- Tests.cpp
+++ Tests.cpp
@@ -9,6 +9,7 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include "HaarWaveletTransform.h"
+#include "UnsignedInteger.h"
 
 const double DOUBLE_CLOSE = 0.0001;
 
@@ -96,6 +97,44 @@ BOOST_AUTO_TEST_CASE( SimpleHaarInverseTransformDifferentWidthHeightTest ) {
 			BOOST_CHECK_CLOSE(testData[(row * testWidth) + column], haarTransform.getItem(row, column), DOUBLE_CLOSE);
 		}
 	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE( UnsignedIntegerTests )
+
+BOOST_AUTO_TEST_CASE( isPowerOfTwoTest ) {
+	unsigned int powersOfTwo[32];
+	powersOfTwo[0] = 1;
+	unsigned int nextPow = 1;
+	for(unsigned int i = 0; i < 31; ++i) {
+		nextPow <<= 1;
+		powersOfTwo[i + 1] = nextPow;
+	}
+
+	for(unsigned int i = 0; i < 32; ++i) {
+		BOOST_CHECK(UnsignedInteger::isPowerOfTwo(powersOfTwo[i]) == true);
+	}
+
+	unsigned int notPowersOfTwo[10] = {0, 3, 130, 6, 97, 2000, 10000, 700, 100000, 25};
+	for(unsigned int i = 0; i < 10; ++i) {
+		BOOST_CHECK(UnsignedInteger::isPowerOfTwo(notPowersOfTwo[i]) == false);
+	}
+}
+
+BOOST_AUTO_TEST_CASE( getClosestPowerOfTwoTest ) {
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(0), 1u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(3), 4u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(5), 8u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(7), 8u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(120), 128u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(16300), 16384u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(16385), 32768u);
+
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(2), 2u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(1024), 1024u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(256), 256u);
+	BOOST_CHECK_EQUAL(UnsignedInteger::getClosestPowerOfTwo(4), 4u);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
